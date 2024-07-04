1cb544d276368055df57766593b458ee6e93a4bf
Nicolas von Trott <nicolasvontrott@gmail.com>
2024-07-04 17:44:53 +0200

    Fixed sql command

--- ChangesetFetcher/SQLiteConnector.py
+++ ChangesetFetcher/SQLiteConnector.py
@@ -2,7 +2,10 @@ import sqlite3
 import logging
 
 from Changeset import Changeset
+from time import strftime
 
+TABLE_NAME_CHANGESETS = 'changesets'
+SQL_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 class SQLiteConnector:
     """
@@ -39,10 +42,10 @@ class SQLiteConnector:
         Creates the table to store the changesets if this table does not already exist in the database.
         :return:
         """
-        table_definition = """CREATE TABLE IF NOT EXISTS changesets(
+        table_definition = f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME_CHANGESETS}(
     id INTEGER PRIMARY KEY,
-    created_at DATE,
-    closed_at DATE,
+    created_at DATETIME,
+    closed_at DATETIME,
     user TEXT,
     user_id INTEGER,
     min_lat REAL,
@@ -53,13 +56,38 @@ class SQLiteConnector:
     changes_count INTEGER
 );"""
         self.__cursor.execute(table_definition)
+        logging.info("Table created successfully")
 
     def insert_changeset(self, changeset: Changeset):
         """
-        Inserts a changeset into the database
+        Inserts a changeset into the database. If a changeset with the given id already exists, it will be ignored.
         :return:
         """
+        created_at = changeset.created_at.strftime(SQL_TIME_FORMAT)
+        if changeset.closed_at is None:
+            closet_at: str = ""
+        else:
+            closet_at: str = changeset.closed_at.strftime(SQL_TIME_FORMAT) or None
+
         self.__cursor.execute(
-            f"INSERT INTO {self.databaseName} VALUES ({changeset.id},{changeset.created_at},{changeset.closed_at},{changeset.user},{changeset.user_id},{changeset.min_lat},{changeset.min_lon},{changeset.max_lat},{changeset.max_lon},{changeset.comments_count},{changeset.changes_count})"
+            f"""INSERT OR IGNORE  INTO {TABLE_NAME_CHANGESETS} VALUES (
+            {changeset.id},
+            \'{created_at}\',
+            \'{closet_at}\',
+            \'{changeset.user}\',
+            {changeset.uid},
+            {changeset.min_lat},
+            {changeset.min_lon},
+            {changeset.max_lat},
+            {changeset.max_lon},
+            {changeset.comments_count},
+            {changeset.num_changes}
+            );
+            """
         )
         self.__connection.commit()
+        logging.info(
+            f"""Either Inserted changeset with id {changeset.id} into table {TABLE_NAME_CHANGESETS} or there 
+                already exists changeset with id {changeset.id}
+            """
+        )
